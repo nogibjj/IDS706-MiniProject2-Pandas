9d15ffb743ad2a324a59a8ffba59b5e726c869bc
Eric Yin <ericyin@EricdeMacBook-Pro.local>
2023-09-10 19:21:16 -0400

    add tests

--- test_main.py
+++ test_main.py
@@ -1,13 +1,9 @@
-"""
-Test goes here
-
-"""
-
 # test_main.py
 
 import unittest
 import pandas as pd
 import main  # Assuming main.py is in the same directory
+
 
 class TestMainMethods(unittest.TestCase):
 
@@ -19,19 +15,79 @@ class TestMainMethods(unittest.TestCase):
             'Score': [85, 89, 78, 90, 82]
         })
 
+        # Creating a sample CSV for testing
+        self.data.to_csv('test_data.csv', index=False)
+
     def test_load_data(self):
-        data = main.load_data('data.csv')
+        data = main.load_data('test_data.csv')
         self.assertTrue(isinstance(data, pd.DataFrame))  # Check if data is a DataFrame
         self.assertNotEqual(len(data), 0)  # Check if DataFrame is not empty
 
+    def test_display_data(self):
+        # Testing display_data function by capturing printed output
+        from io import StringIO
+        import sys
+
+        backup = sys.stdout
+
+        # Switch stdout to capture printed output
+        sys.stdout = StringIO()
+        main.display_data(self.data)
+        output = sys.stdout.getvalue()
+
+        sys.stdout = backup  # Restore original stdout
+        self.assertIn("Data preview:", output)
+        self.assertIn("25", output)
+
+    def test_display_basic_statistics(self):
+        # Similar to test_display_data, capture output and check if required data is printed
+        from io import StringIO
+        import sys
+
+        backup = sys.stdout
+
+        sys.stdout = StringIO()
+        main.display_basic_statistics(self.data)
+        output = sys.stdout.getvalue()
+
+        sys.stdout = backup
+        self.assertIn("Basic Descriptive Statistics:", output)
+        self.assertIn("mean", output)
+
     def test_display_median(self):
-        medians = {
-            'Age': 35.0,
-            'Salary': 60000.0,
-            'Score': 85.0
-        }
-        for column in self.data.columns:
-            self.assertEqual(self.data[column].median(), medians[column])
+        # Again, capture output and verify
+        from io import StringIO
+        import sys
+
+        backup = sys.stdout
+
+        sys.stdout = StringIO()
+        main.display_median(self.data)
+        output = sys.stdout.getvalue()
+
+        sys.stdout = backup
+        self.assertIn("Median of Age: 35.0", output)
+
+    def test_display_mode(self):
+        # Similarly, capture output and verify for mode
+        from io import StringIO
+        import sys
+
+        backup = sys.stdout
+
+        sys.stdout = StringIO()
+        main.display_mode(self.data)
+        output = sys.stdout.getvalue()
+
+        sys.stdout = backup
+        self.assertIn("Mode of Age: 25", output)
+
+    def tearDown(self):
+        # This method will be executed after each test method
+        # Deleting the test CSV file after tests
+        import os
+        os.remove('test_data.csv')
+
 
 if __name__ == '__main__':
     unittest.main()
