3baf74882beb28cb15f1a9d1cd8e92da44654f19
Eric Yin <ericyin@EricdeMacBook-Pro.local>
2023-09-10 19:01:34 -0400

    write pandas script

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 """
 Main cli or app entry point
+
+
+# Note: This script assumes that all columns in data.csv are numeric.
+# If there are non-numeric columns, you'd need to handle or exclude them when computing these statistics.
 """
 
-def add(a, b):
-    return a + b
+import pandas as pd
 
-def subtract(a, b):
-    return a - b
+def load_data(filename):
+    return pd.read_csv(filename)
 
-def multiply(a, b):
-    return a*b
 
-def divide(a, b):
-    return a/b
+def display_data(data):
+    print("Data preview:")
+    print(data.head())
+    print("\n")
+
+
+def display_basic_statistics(data):
+    print("Basic Descriptive Statistics:")
+    print(data.describe())
+    print("\n")
+
+
+def display_median(data):
+    print("Median of the columns:")
+    for column in data.columns:
+        print(f"Median of {column}: {data[column].median()}")
+    print("\n")
+
+
+def display_mode(data):
+    print("Mode of the columns:")
+    for column in data.columns:
+        mode_value = data[column].mode()[0]
+        print(f"Mode of {column}: {mode_value}")
+    print("\n")
+
+
+def main():
+    data = load_data('data.csv')
+
+    display_data(data)
+    display_basic_statistics(data)
+    display_median(data)
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    print("Testing:")
+    main()
--- test_main.py
+++ test_main.py
@@ -3,24 +3,35 @@ Test goes here
 
 """
 
-from main import add, subtract, multiply, divide
+# test_main.py
 
+import unittest
+import pandas as pd
+import main  # Assuming main.py is in the same directory
 
-def test_add():
-    assert add(1, 2) == 3
+class TestMainMethods(unittest.TestCase):
 
-def test_subtract():
-    assert subtract(1, 2) == -1
+    def setUp(self):
+        # This method will be executed before each test method, setting up the data for the tests
+        self.data = pd.DataFrame({
+            'Age': [25, 30, 35, 40, 45],
+            'Salary': [50000, 55000, 60000, 65000, 70000],
+            'Score': [85, 89, 78, 90, 82]
+        })
 
-def test_multiply():
-    assert multiply(1, 2) == 2
+    def test_load_data(self):
+        data = main.load_data('data.csv')
+        self.assertTrue(isinstance(data, pd.DataFrame))  # Check if data is a DataFrame
+        self.assertNotEqual(len(data), 0)  # Check if DataFrame is not empty
 
-def test_divide():
-    assert divide(2, 1) == 2
+    def test_display_median(self):
+        medians = {
+            'Age': 35.0,
+            'Salary': 60000.0,
+            'Score': 85.0
+        }
+        for column in self.data.columns:
+            self.assertEqual(self.data[column].median(), medians[column])
 
-if __name__ == "__main__":
-    test_add()
-    test_subtract()
-    test_multiply()
-    test_divide()
-    print("Everything passed")
+if __name__ == '__main__':
+    unittest.main()
